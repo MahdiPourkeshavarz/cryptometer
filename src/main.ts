e44388365cb0172b2c4855a69575f3424c4177bb
mahdi keshavarz <69865082+ma-pk1996@users.noreply.github.com>
2025-10-09 14:48:46 +0330

    feat: render error

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 10000;
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Application is running on: http://0.0.0.0:${port}`);
 }
 bootstrap();
