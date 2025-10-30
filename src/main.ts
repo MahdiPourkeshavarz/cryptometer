984839d26355cd4b2aa6b481ebc05a4a3b456517
Mahdi Pourkeshavarz <69865082+MahdiPourkeshavarz@users.noreply.github.com>
2025-10-30 11:34:13 +0330

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://cryptometer-web.vercel.app',
+    origin: 'https://pulse-ir.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
